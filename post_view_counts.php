acfa9e83542fd2720fc67620cd5c4b707484666d
Ajay Singh <108493052+techajju@users.noreply.github.com>
2022-12-19 21:12:05 +0530

    Update post_view_counts.php

--- post_view_counts.php
+++ post_view_counts.php
@@ -16,3 +16,30 @@ function add_post_views($post_id){ //call this functiob on single.php
 	}
    
 }
+
+//show populer post 
+
+$arg = array(
+	'post_type' =>'post',
+	'posts_per_page' => 3,
+	'meta_key' =>'post_views_count',
+	'orderby' =>'meta_value_num',
+	'order' =>'desc',
+ 
+
+);
+
+
+$query = new WP_Query($arg);
+
+foreach ($query->posts as $key => $value) {
+	$post_id = $value->ID;
+	$count = get_post_meta( $post_id, 'post_views_count' ); 
+	echo $value->post_title. " Views ". $count[0];
+	echo "<br>";
+
+
+}
+
+
+
